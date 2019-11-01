d7c65031a406cbecd73566f55454fe3204f683e8
Brian Odermatt <brian.odermatt@intergga.ch>
2019-11-01 19:17:33 +0100

    EEPROM read-write floats

--- arduino/eeprom.ino
+++ arduino/eeprom.ino
@@ -3,6 +3,9 @@
  */
 #include <EEPROM.h>
 
+/*
+ * write
+ */
 // write a 4 byte long to the eeprom at the specified address (until addr+3)
 void eepromWriteLong(int p_address, long p_value) {
   if (p_address <= EEPROM.length() - 4) {
@@ -17,6 +20,21 @@ void eepromWriteLong(int p_address, long p_value) {
   }
 }
 
+// write a 4 byte float to the eeprom at the specified address (until addr+3)
+void eepromWriteFloat(int p_address, float p_value) {
+  if (p_address <= EEPROM.length() - 4) {
+    byte bytes[4];
+    *(float*)(bytes) = p_value;
+    EEPROM.write(p_address + 0, bytes[0]);
+    EEPROM.write(p_address + 1, bytes[1]);
+    EEPROM.write(p_address + 2, bytes[2]);
+    EEPROM.write(p_address + 3, bytes[3]);
+  }
+}
+
+/*
+ * read
+ */
 // read a 4 byte long from the eeprom at the specified address (until addr+3)
 unsigned long eepromReadLong(int p_address) {
   if (p_address <= EEPROM.length() - 4) {
@@ -30,6 +48,19 @@ unsigned long eepromReadLong(int p_address) {
       (((long)b2 << 16) & 0xFF0000) + 
       (((long)b3 << 24) & 0xFF000000)
     );
+  }
+  else return 0;
+}
+
+// read a 4 byte float from the eeprom at the specified address (until addr+3)
+unsigned long eepromReadFloat(int p_address) {
+  if (p_address <= EEPROM.length() - 4) {
+    byte bytes[4];
+    bytes[0] = EEPROM.read(p_address + 0);
+    bytes[1] = EEPROM.read(p_address + 1);
+    bytes[2] = EEPROM.read(p_address + 2);
+    bytes[3] = EEPROM.read(p_address + 3);  
+    return *(float*)(bytes);
   }
   else return 0;
 }
